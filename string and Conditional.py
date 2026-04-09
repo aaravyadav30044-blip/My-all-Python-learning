0469a1ccee3947c88229e1bd4102465f1fd2daeb
aarav Yadav <aaravyadav30044@gmail.com>
2026-04-09 20:24:42 +0530

    Add files via upload

--- string and Conditional.py
+++ string and Conditional.py
@@ -1,5 +1,5 @@
-s = input("enter the text of line: ")
-if s == "":
-    print("empty")
-else:
-    print("not empty")
+s = input("enter the text: ")
+rev = ""
+for i in range(len(s)-1,-1,-1):
+    rev = rev + s[i]
+print(rev,sep="")
